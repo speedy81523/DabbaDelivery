0aab88b5d363ab47e108641e050f23f7a0f8b899
speedy81523 <tylerjamestan@gmail.com>
2026-07-27 10:04:33 +0800

    Update game.js

--- dabbaDelivery/game.js
+++ dabbaDelivery/game.js
@@ -99,7 +99,7 @@ function updateScoreDisplay() {
 //get box duration (movement speed ramps up)
 function getBoxDuration() {
   const elapsedFraction = 1 - timeLeft / starting_seconds;
-  const duration = 13 - elapsedFraction * 4;
+  const duration = 12 - elapsedFraction * 4;
   return Math.max(6, Math.round(duration));
 }
 
