12fcb88876b2a146a1f92dde39f91262ea53f471
speedy81523 <tylerjamestan@gmail.com>
2026-07-28 18:14:50 +0800

    Update game.js

--- dabbaDelivery/game.js
+++ dabbaDelivery/game.js
@@ -631,8 +631,7 @@ function spawnBox(){
   `;
   converyorTrack.appendChild(el);
   setTimeout(()=>el.classList.remove("pop-in"),350);
-  
-   el.style.top = `${getBoxTopOffset(lane)}px`;
+  el.style.top = `${getBoxTopOffset(lane)}px`;
   //box details
   const box = {
     id,
